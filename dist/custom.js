4cb7eb1520102bd364dac6d42d5d798d817afc06
vincent choqueuse <vincentchoqueuse@pc34.home>
2019-02-09 16:05:44 +0100

    first commit

--- dist/custom.js
+++ dist/custom.js
@@ -42,7 +42,7 @@ $(function () {
             den = [(1/(w0*w0)),2*m/w0,1];
             num = num.join(",");
             den = den.join(",");
-            T_max =10/(w0);
+            T_max =20/(w0);
             url = "https://bzhjqstm3f.execute-api.eu-west-1.amazonaws.com/dep?num="+num+"&den="+den+"&tmax="+T_max;
                     
             $("#btn_submit").prop("disabled",true);
